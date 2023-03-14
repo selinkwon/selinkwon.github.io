7a2fefa7dee38c13e625cfafbde25924547e18e0
tpfls123v <tpfls123v@naver.com>
2023-03-14 17:41:50 +0900

    Updata OMOK

--- OMOK/omok.js
+++ OMOK/omok.js
@@ -1,64 +1,72 @@
 const map = document.querySelector("#map");
-const size = 10;
+const size = 9;
 let win = 0;
 let turn = 1;
-let mark = [];
+const mark = [];
 
 const [one, two] = [
     "https://em-content.zobj.net/thumbs/240/whatsapp/326/black-circle_26ab.png","https://em-content.zobj.net/thumbs/240/whatsapp/326/white-circle_26aa.png"
 ];
 setMap();
+playsetMap();
 function setMap() {
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
+    for (let i = 0; i < size+1; i++) {
+        for (let j = 0; j < size+1; j++) {
             const id = `y${i}X${j}`;
             const box = document.createElement("div");
-            const img = document.createElement("img");
             box.setAttribute("id", id);
             box.setAttribute("class", "box");
-            mark.push(0);
+            map.append(box);
+        }
+    }
+    
+}
+
+function playsetMap(){
+    const map1 = document.createElement("div");
+    map.append(map1);
+    map1.setAttribute("class", "map1");
+    for (let i = 0; i < size; i++) {
+        const marking = [];
+        mark.push(marking);
+        for (let j = 0; j < size; j++) { 
+            marking.push(0);           
+            const box = document.createElement("div");
+            box.setAttribute("class", "box1");
+            const img = document.createElement("img");
+            
+            console.log(mark[i][j]);
             box.addEventListener("click", e => {
-                if (mark[i] === 0,mark[j]===0) {
+                if (mark[i][j] === 0) {
                     if (turn === 1) {
                         img.setAttribute("src", one);
                     }
                     else {
                         img.setAttribute("src", two);
                     }
-                    mark[i] = turn;
+                    mark[i][j] = turn;
                     checkWin();
                     turn = turn == 1 ? 2 : 1;
                     box.append(img);
                 }
             })
-            map.append(box);
+            map1.append(box);
         }
     }
-    const map1 = document.createElement("div");
-    const body = document.querySelector("body");
-    body.append(map1);
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {            
-            const box = document.createElement("div");
-            box.setAttribute("class", "box1");
-            
-            map.append(box);
-        }
-    }
-
+    
 }
 
 function checkWin(){
     for(let i=0;i<size;i++) {
         let cnt = 0;
         for(let j=0;j<size;j++) {
-            if(map[i][j] == turn) {
+            if(mark[i][j] === turn) {
                 cnt ++;
             }
             else {
                 cnt = 0;
             }
-            if (cnt == 5) {
+            if (cnt === 5) {
                 win = turn;
             }
         }
@@ -67,13 +75,13 @@ function checkWin(){
     for(let i=0;i<size;i++) {
         let cnt = 0;
         for(let j=0;j<size;j++) {
-            if(map[j][i] == turn) {
+            if(mark[j][i] === turn) {
                 cnt ++;
             }
             else {
                 cnt = 0;
             }
-            if (cnt == 5) {
+            if (cnt === 5) {
                 win = turn;
             }
         }
@@ -81,13 +89,13 @@ function checkWin(){
 
     for(let i=0;i<size-4;i++) {
         for(let j=0;j<size-4;j++) {
-            if(map[i][j] == turn) {
+            if(mark[i][j] === turn) {
                 let cnt = 0;
                 for(let k=0;k<5;k++) {
-                    if(map[i+k][j+k]==turn)
+                    if(mark[i+k][j+k]===turn)
                         cnt ++;
                 }
-                if(cnt == 5) {
+                if(cnt === 5) {
                     win = turn;
                 }
             }
@@ -96,17 +104,21 @@ function checkWin(){
 
     for(let i=4;i<size;i++) {
         for(let j=0;j<size-4;j++) {
-            if(map[i][j] == turn) {
+            if(mark[i][j] === turn) {
                 let cnt = 0;
                 for(let k=0;k<5;k++) {  
-                     if(map[i-k][j+k]==turn)
+                     if(mark[i-k][j+k]===turn)
                         cnt ++;
                 }
-                if(cnt == 5) {
+                if(cnt === 5) {
                     win = turn;
                 }
             }
         }
+    }
+
+    if (win != 0) {
+        alert(`P${win} WIN!!!`)
     }
 }
 
